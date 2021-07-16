5abbd0551b204fa597b2618d5f40d880bc422ced
danceoval <daniel.sohval@gmail.com>
2021-07-16 17:08:36 -0400

    adds model test

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const path = require('path');
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: path.join(__dirname, 'db.sqlite'),
+    logging: false
 });
 
 module.exports = {sequelize}
