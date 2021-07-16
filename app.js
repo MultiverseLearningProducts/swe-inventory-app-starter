385b32192b6cfbb81cc9ddaddc0f5d0296f73dd5
= <=>
2021-07-16 11:43:45 -0700

    Adds DB, Sauce Model, Server

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const Handlebars = require('handlebars');
+const expressHandlebars = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+
+const {sequelize} = require('./db');
+const {Sauce} = require('./models');
+
+const PORT = 3000;
+
+const app = express();
+
+// setup our templating engine
+const handlebars = expressHandlebars({
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+app.engine('handlebars', handlebars);
+app.set('view engine', 'handlebars');
+
+// serve static assets from the public/ folder
+app.use(express.static('public'));
+
+const seedDb = async () => {
+
+    await sequelize.sync({ force: true });
+
+    const sauces = [
+        {name : 'Sriracha', image : '/img/Sriracha.gif'},
+        {name : 'Franks', image: '/img/Franks.gif'},
+        {name : 'Tobasco', image: '/img/Tobasco.gif'}
+    ]
+
+    const saucePromises = sauces.map(sauce => Sauce.create(sauce))
+    await Promise.all(saucePromises)
+    console.log("db populated!")
+}
+
+seedDb();
+
+app.get('/', async (req, res) => {
+    const allHotSauces = await Sauce.findAll()
+    res.render('HELLO WORLD', {allHotSauces});
+})
+
+
+app.listen(PORT, () => {
+    sequelize.sync({force: true});
+    console.log(`Your server is running on http://localhost:${PORT}`);
+})
