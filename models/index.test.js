67d769b14ed9adb213250fa5df14c66bf35300c8
Zeru <zeruna@ad.vzwcorp.com>
2021-07-21 13:16:09 -0500

    testt

--- models/index.test.js
+++ models/index.test.js
@@ -6,13 +6,8 @@ describe('Item Model', () => {
 		await sequelize.sync({force: true})
 	});
 
-<<<<<<< HEAD
-	test('can create a item', async() => {
-		const testItem = await Sauce.create({name : 'Cholula'})
-=======
 	test('can create a Item', async() => {
 		const testItem = await Item.create({name : 'Cholula'})
->>>>>>> 75c28009afee90fccbb5febdac0964081634ef3b
 		expect(testItem.name).toBe('Cholula')
 	})
 
