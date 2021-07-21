82f354d332691a7b1eaf0bc4a1a4b569c5955bbf
danceoval <daniel.sohval@gmail.com>
2021-07-21 10:32:32 -0400

    fix typo

--- seed.js
+++ seed.js
@@ -9,12 +9,12 @@ const seed = async () => {
 
     await sequelize.sync({ force: true });
 
-    const seedPath = path.join(__dirname, 'sauces.json');
-    const buffer = await fs.readFile(seedPath);
-    const {data} = JSON.parse(String(buffer));
+    const seedPath = path.join(__dirname, 'sauces.json'); // creates path to seed data
+    const buffer = await fs.readFile(seedPath); // reads json
+    const {data} = JSON.parse(String(buffer)); //parses data
 
-    const saucePromises = data.map(sauce => Sauce.create(sauce))
-    await Promise.all(saucePromises)
+    const dataPromises = data.map(item => Sauce.create(item))
+    await Promise.all(dataPromises)
     console.log("db populated!")
 }
 
