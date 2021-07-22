0cc58025dee7a696ffa51fcf6210e64d18cb1969
DamonCrutchfield <crutchfielddamond@gmail.com>
2021-07-22 11:05:40 -0500

    Add new models for User, Warehouse, Inventory, and Supplier

--- models/index.js
+++ models/index.js
@@ -1,11 +1,12 @@
-
-
-
 const {Sequelize, DataTypes, Model} = require('sequelize')
 const {sequelize} = require('../db')
 
 
 class Item extends Model {}
+class User extends Model {}
+class Warehouse extends Model {}
+class Inventory extends Model {}
+class Supplier extends Model {}
 
 Item.init({
     name: DataTypes.STRING,
@@ -18,4 +19,39 @@ Item.init({
     timestamps: false,
 });
 
-module.exports = {Item};
+User.init({
+    name: DataTypes.STRING,
+    role: DataTypes.STRING,
+    root: DataTypes.BOOLEAN
+}, {
+    sequelize,
+    timestamps: false,
+});
+
+Warehouse.init({
+    name: DataTypes.STRING,
+    location: DataTypes.STRING
+}, {
+    sequelize,
+    timestamps: false,
+});
+
+Inventory.init({
+    name: DataTypes.STRING,
+    amount: DataTypes.STRING,
+    orderMore: DataTypes.BOOLEAN
+}, {
+    sequelize,
+    timestamps: false,
+});
+
+Supplier.init({
+    name: DataTypes.STRING,
+    location: DataTypes.STRING,
+    buyer: DataTypes.BOOLEAN
+}, {
+    sequelize,
+    timestamps: false,
+});
+
+module.exports = {Item, User, Warehouse, Inventory, Supplier};
