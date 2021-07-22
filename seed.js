b1c56bbcd2062e043ba6da2b394b0624f36d7b86
Judith <25380136+jjliem@users.noreply.github.com>
2021-07-22 14:18:23 -0500

    add code to seed from json, update Magnum URL

--- seed.js
+++ seed.js
@@ -15,9 +15,17 @@ const seed = async () => {
     const seedPath = path.join(__dirname, 'icecream.json'); // creates path to seed data
     const buffer = await fs.readFile(seedPath); // reads json
     const {data} = JSON.parse(String(buffer)); //parses data
+    
+    for (const brandData of data) {
+        const brand = await Brand.create(brandData);
+        for (const flavorData of brandData.flavors) {
+            const flavor = await Flavor.create(flavorData);
+            await brand.addFlavor(flavor);
+        }
+    }
 
-    const dataPromises = data.map(item => Brand.create(item))
-    await Promise.all(dataPromises)
+    // const dataPromises = data.map(item => Brand.create(item))
+    // await Promise.all(dataPromises)
     console.log("db populated!")
 }
 
