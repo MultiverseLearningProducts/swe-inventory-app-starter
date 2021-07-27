e1e5d81fde5523623f72b96924d1168f03631f2e
danceoval <daniel.sohval@gmail.com>
2021-07-27 13:23:09 -0400

    updates link

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const {sequelize} = require('./db');
 const {Sauce} = require('./models');
 const seed = require('./seed')
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
