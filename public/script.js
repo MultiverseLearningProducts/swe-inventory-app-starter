4985fa08fa2510508922fe81a050142c0a309947
DamonCrutchfield <crutchfielddamond@gmail.com>
2021-07-29 09:53:34 -0500

    Modify delete button functionality to immediately reflect data removed from database

--- public/script.js
+++ public/script.js
@@ -1,7 +1,12 @@
 const button = document.getElementById('btn');
 const itemName = document.getElementById('item-name');
 const deleteButton = document.getElementById('delete-button');
+const item = document.getElementById(`item-${deleteButton.dataset.id}`)
 
-deleteButton.addEventListener('click', async (e) => {
-    await fetch(`/remove-item/${deleteButton.dataset.id}`, {method: 'DELETE'})
+deleteButton.addEventListener('click', (e) => {
+    const deleteItem = async function() {
+       await fetch(`/remove-item/${deleteButton.dataset.id}`, {method: 'DELETE'}) 
+    }
+    deleteItem();
+    location.reload();
 })
